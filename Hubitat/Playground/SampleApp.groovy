464d7962326c96ef3844a264c3dd74415a2afeac
precompiler <lishengyin.msn@hotmail.com>
2021-01-27 21:21:35 +0000

    add preference sample code

--- Hubitat/Playground/SampleApp.groovy
+++ Hubitat/Playground/SampleApp.groovy
@@ -15,19 +15,79 @@ preferences {
         }
     }
     
-    page(name: "pg2", title: "Page Two", uninstall: true, install: true) {
+    page(name: "pg2", title: "Page Two", nextPage: "pg3", uninstall: true) {
         section("Advanced Configurations") {
             paragraph "This is a long description that rambles on and on and on..."
+
             href(name: "hrefNotRequired",
              title: "Hubitat",
              required: false,
              style: "external",
              url: "https://hubitat.com/",
              description: "tap to view hubitat website")
+            
+            input "modes", "mode", title: "only when mode is", multiple: true, required: false
+
+            input(name: "color", type: "enum", title: "Color", options: ["Red","Green","Blue","Yellow"], multiple: false)
+
+            input("myDevice", "device.dummy", hideWhenEmpty: true)
         }
+    }
+
+    page(name: "pg3", title: "Page Three", uninstall: true, nextPage: "pg4") {
+        section {
+            input("sensorType", "enum", options: [
+                "contactSensor":"Open/Closed Sensor",
+                "motionSensor":"Motion Sensor",
+                "switch": "Switch",
+                "moistureSensor": "Moisture Sensor"])
+
+            input("actuatorType", "enum", options: [
+                "switch": "Light or Switch",
+                "lock": "Lock"]
+            )
+        }
+    }
+
+    page(name: "pg4", title: "Page Four", uninstall: true, install: true) {
+        section {
+            input(name: "sensor", type: "capability.$sensorType", title: "If the $sensorType device")
+            input(name: "sensorAction", type: "enum", title: "is", options: attributeValues(sensorType))
+        }
+        section {
+            input(name: "actuator", type: "capability.$actuatorType", title: "Set the $actuatorType")
+            input(name: "actuatorAction", type: "enum", title: "to", options: actions(actuatorType))
+         }
+
     }
 }
 
+
+private attributeValues(attributeName) {
+    switch(attributeName) {
+        case "switch":
+            return ["on","off"]
+        case "contactSensor":
+            return ["open","closed"]
+        case "motionSensor":
+            return ["active","inactive"]
+        case "moistureSensor":
+            return ["wet","dry"]
+        default:
+            return ["UNDEFINED"]
+    }
+}
+
+private actions(attributeName) {
+    switch(attributeName) {
+        case "switch":
+            return ["on","off"]
+        case "lock":
+            return ["lock","unlock"]
+        default:
+            return ["UNDEFINED"]
+    }
+}
 
 def installed() {
     // called when the app is installed for the first time
