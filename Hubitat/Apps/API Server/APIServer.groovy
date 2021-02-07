f41917b8b85bfa2c94778b3f0fbd4caa5356aabc
precompiler <lishengyin.msn@hotmail.com>
2021-02-07 23:37:18 +0000

    add app stub

---
+++ Hubitat/Apps/API Server/APIServer.groovy
@@ -0,0 +1,39 @@
+import groovy.json.JsonBuilder
+
+definition(
+    name: "API Server",
+    namespace: "precompiler",
+    author: "Richard Li",
+    description: "Control Devices",
+    category: "API",
+    iconUrl: "",
+    iconX2Url: "",
+    iconX3Url: ""
+)
+
+mappings {
+    path("/greeting") { action: [GET: "greeting"]}
+}
+
+def greeting() {
+    log.info("========hello!==========")
+    render(contentType: "application/json", data: new JsonBuilder(["msg": "hello"]).toPrettyString())
+}
+
+
+def installed() {
+    initialize()
+}
+
+def updated() {
+    unsubscribe()
+    initialize()
+}
+
+def initialize() {
+    if (!state.accessToken) {
+        createAccessToken()
+    }
+    // for test only
+    log.info("${getLocalApiServerUrl()}/${app.id}/greeting?access_token=${state.accessToken}")
+}
