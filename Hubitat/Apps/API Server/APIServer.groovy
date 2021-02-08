a2910d32c26810c14dbc2df1012811d6f5ed4bfc
precompiler <lishengyin.msn@hotmail.com>
2021-02-08 23:34:03 +0000

    add APIs to list and get hub info

--- Hubitat/Apps/API Server/APIServer.groovy
+++ Hubitat/Apps/API Server/APIServer.groovy
@@ -12,20 +12,42 @@ definition(
 )
 
 mappings {
-    path("/greeting") { action: [GET: "greeting"]}
+    path("/hubs") { action: [GET: "getHubs"]}
+    path("/hubs/:id") { action: [GET: "getHubInfo"]}
 }
 
 preferences {
     section("Logging Setting") {
-        input("logEnable", "bool", "Enable logging?", required: false, default: false)
+        input(name: "logEnable", type: "bool", title: "Enable logging?", required: false, default: false)
     }
 }
 
-def greeting() {
-    log.info("========hello!==========")
-    render(contentType: "application/json", data: new JsonBuilder(["msg": "hello"]).toPrettyString())
+def getHubs() {
+    def hubList = []
+    location.hubs?.each {
+        hubList.add([
+            "id": it.id,
+            "name": it.name
+        ])
+    }
+    render(contentType: APPLICATION_JSON, data: new JsonBuilder(hubList).toPrettyString())
 }
 
+def getHubInfo() {
+    def hub = location.hubs?.find{String.valueOf(it.id) == params.id}
+    if(!hub) {
+        render(status: 404, contentType: APPLICATION_JSON, data: '')
+    } else {
+        def ret = [
+            "id": hub.id,
+            "name": hub.name,
+            "localIP": hub.localIP,
+            "firmwareVersion": hub.firmwareVersionString,
+            "uptime": "${hub.uptime/60/60}h"
+        ]
+        render(contentType: APPLICATION_JSON, data: new JsonBuilder(ret).toPrettyString())
+    }
+}
 
 def installed() {
     initialize()
@@ -44,3 +66,5 @@ def initialize() {
     log.info("${getLocalApiServerUrl()}/${app.id}/greeting?access_token=${state.accessToken}")
 }
 
+
+final String APPLICATION_JSON = 'application/json'
